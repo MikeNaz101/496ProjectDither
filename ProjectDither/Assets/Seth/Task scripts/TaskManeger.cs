2611eb80e7d59607639c118f13d5efb983291735
ZetaSense <zetasense2@gmail.com>
2025-03-06 23:56:47 -0500

    Testing commit
    :)

---
+++ ProjectDither/Assets/Seth/Task scripts/TaskManeger.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class TaskManeger : MonoBehaviour
+{
+
+    //public bool[SelectTasks] = Random.Range (0,6);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       // SelectTasks.random.list;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
