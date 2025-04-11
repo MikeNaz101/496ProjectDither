185e700315a8b7644dcad0bba4bcdfde975609d5
cmccar011 <157764522+cmccar011@users.noreply.github.com>
2025-04-10 20:03:34 -0400

    me make smoke
    cor make smoke

---
+++ ProjectDither/Assets/Cory/CorScripts/phonePickup.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class phonePickup : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
