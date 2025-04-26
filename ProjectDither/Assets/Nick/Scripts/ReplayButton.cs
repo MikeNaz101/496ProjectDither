139387177ba33394702c4ece71b144f88d4cae8f
Nick <nwpopie@icloud.com>
2025-04-26 11:10:15 -0400

    WinGameScreen

---
+++ ProjectDither/Assets/Nick/Scripts/ReplayButton.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReplayButton : MonoBehaviour
+{
+    public void onPress()
+    {
+        SceneManager.LoadScene("Mike/MikesWeeping Angel 2");
+    }
+}
